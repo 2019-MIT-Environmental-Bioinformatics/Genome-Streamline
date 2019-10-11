8cf5a308c8cbedb1e26756d745d29211470d2332
Cory Berger <cberger@whoi.edu>
2019-10-11 11:51:28 -0400

    Updates prodigal script

--- scripts/Prodigal-1.sh
+++ scripts/Prodigal-1.sh
@@ -4,19 +4,19 @@
 #SBATCH --mail-type=END             	# Mail events (BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=ngermolus@whoi.edu  # Where to send mail
 #SBATCH --ntasks=1                  	# Run on a single CPU
-#SBATCH --cpus-per-task=36		# Use x cores on node
+#SBATCH --cpus-per-task=2		# Use x cores on node
 #SBATCH --mem=1gb                   	# Job memory request
 #SBATCH --time=06:00:00             	# Time limit hrs:min:sec
 #SBATCH --output=prodigal_%j.log  	# Standard output/error
 
-export OMP_NUM_THREADS=36
+export OMP_NUM_THREADS=2
 module load bio prodigal
 
 for file in $(find /vortexfs1/omics/env-bio/collaboration/genome-streamlining/data/SAGs/ -name *fna.gz)
 do
 
 filename=$(basename ${file} .fna.gz)
-prodigal -i ${file} -o ${filename}_coords.gbk -a ${filename}_proteins.faa
+zcat $file | prodigal -o ${filename}_coords.gbk -a ${filename}_proteins.faa
 
 done
 
